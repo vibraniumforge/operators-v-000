a9e5f2fcea278c4fd268dd7c9cac06a2ada839b7
Greg Morgan <gregm9999@gmail.com>
2018-11-01 23:41:02 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -3,16 +3,11 @@ def unsafe?(speed)
 		true
 	elsif speed<40
 		true
-	elsif speed<=40 && speed>=60
+	elsif speed<=40 || speed>=60
 		false
 	end
 end
 
-
-
 def not_safe?(speed)
-	
+	speed >=60 || speed<=40 ? true : false
 end
-	
-
-
